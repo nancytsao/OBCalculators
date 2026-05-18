70fb163d5caa614704fb2dd6eafca9374029be29
nancytsao <nancy.hl.tsao@gmail.com>
2026-05-17 20:14:05 -0700

    Update OB Test
    Update NIPT 2 timeframes and adding NIPT & T3 repeat CBC Tests

--- js/Test.js
+++ js/Test.js
@@ -1,30 +1,38 @@
 var TESTS = [];
 
-TESTS[0] = new Test("SIPS 1",
+TESTS[0] = new Test("NIPT",
+					new Timeframe(new GestationalAge(10, 0), null),
+					null
+					);
+TESTS[1] = new Test("SIPS 1",
 					new Timeframe(new GestationalAge(9, 0), new GestationalAge(13, 6)),
 					new Timeframe(new GestationalAge(10, 0), new GestationalAge(11, 6))
 					);
-TESTS[1] = new Test ("Nuchal Translucency",
+TESTS[2] = new Test ("Nuchal Translucency",
 					new Timeframe(new GestationalAge(11, 0), new GestationalAge(13, 6)),
 					new Timeframe(new GestationalAge(12, 0), new GestationalAge(13, 3))
 					);
-TESTS[2] = new Test ("SIPS 2 / QUAD / NSAFP",
+TESTS[3] = new Test ("SIPS 2 / QUAD ",
 					new Timeframe(new GestationalAge(15, 0), new GestationalAge(20, 6)),
-					new Timeframe(new GestationalAge(15, 2), new GestationalAge(16, 6))
-					);
-TESTS[3] = new Test ("2nd Trimester Ultrasound",
-					new Timeframe(new GestationalAge(18, 0), null),
-					null
+					new Timeframe(new GestationalAge(15, 2), new GestationalAge(16, 0))
 					);
 TESTS[4] = new Test("Amniocentesis",
 					new Timeframe(new GestationalAge(15, 0), new GestationalAge(20, 0)),
 					null
 					);
-TESTS[5] = new Test ("Gestational Diabetes Screening",
+TESTS[5] = new Test ("Detailed Ultrasound",
+					new Timeframe(new GestationalAge(20, 0), null),
+					null
+					);
+TESTS[6] = new Test ("Gestational Diabetes Screening",
 					new Timeframe(new GestationalAge(24, 0), new GestationalAge(28, 0)),
 					null
 					);
-TESTS[6] = new Test ("GBS Swab",
+TESTS[7] = new Test ("Third Trimester CBC",
+					new Timeframe(new GestationalAge(30, 0), null),
+					null
+					);
+TESTS[8] = new Test ("GBS Swab",
 					new Timeframe(new GestationalAge(35, 0), new GestationalAge(37, 0)),
 					null
 					);
