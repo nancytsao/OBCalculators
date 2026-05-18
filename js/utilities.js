2e2219dd89988f670063f1656320957d7e7756bc
nancytsao <nancy.hl.tsao@gmail.com>
2026-05-17 21:45:16 -0700

    Update highlighting function

--- js/utilities.js
+++ js/utilities.js
@@ -23,7 +23,7 @@ function resetSpans(document) {
 
 function setHighlight(fieldId, document) {
 	var field = document.getElementById(fieldId);
-	field.style.border = "2px solid yellow";
+	field.style.border = "4px solid blue";
 }
 
 function removeHighlight(fieldId, document) {
